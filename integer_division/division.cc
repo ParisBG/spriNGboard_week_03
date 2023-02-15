e09233dc888594e9c27bdf0918d06e6cdff91f21
paris_ngc <paris@ngc.com>
2023-02-14 19:13:14 -0600

    Completed int div

--- integer_division/division.cc
+++ integer_division/division.cc
@@ -1,40 +1,59 @@
 #include <iomanip>
 #include <iostream>
-
 using namespace std;
 
-int main ()
-{
-  //
+void createDivTable(bool manualRound){
   // Output the header lines.
-  //
-
   cout << setw(5) << "";
-  for (int32_t denom=1; denom<=10; ++denom) {
+  
+  //Create column headers and show positive symbol for each
+  for (int32_t denom = 1; denom <= 10; ++denom) {
     cout << setw(4) << showpos << denom;
   }
-  cout << endl;
 
+  //Create the intitial dash
+  cout << endl;
   cout << setfill('-') << setw(5) << "";
-  for (int32_t denom=1; denom<=10; ++denom) {
+ 
+  //Fill the remaining blank space with dashes using setfill
+  for (int32_t denom = 1; denom <= 10; ++denom) {
     cout << setfill('-') << setw(4) << "";
   }
+  
   cout << endl;
   cout << setfill(' '); // need to reset the fill character
 
-  //
-  // Fill in the table rows.
-  //
+  int32_t result = 0;
 
-  for (int32_t numer=1; numer<=10; ++numer)
-  {
+  for (int32_t numer = 1; numer <= 10; ++numer){
     cout << setw(3) << showpos << numer << ": ";
-    for (int32_t denom=1; denom<=10; ++denom)
-    {
-      int32_t result = numer/denom;
+
+    for (int32_t denom = 1; denom <= 10; ++denom){
+     
+      if (manualRound == false){
+        result = numer/denom;
+      } else {
+        //We could fix the int div truncation by manually rounding to the nearest int
+        //This can be done be using C++'s int division to divide the denominator in half
+        //Adding that result back to numer and dividing the total by the original denom will effectively round
+        result = (numer + (denom / 2)) / denom;
+      } 
       cout << setw(4) << result;
     }
     cout << endl;
   }
 }
 
+
+int main(){
+  cout << "Table showing truncated int div results:" << endl;
+  createDivTable(false);
+  cout << endl;
+
+  cout << "Table showing manually rounded int div results:" << endl;
+  createDivTable(true);
+  cout << endl;
+
+  return 0;
+
+}
